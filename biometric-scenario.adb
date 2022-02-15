7af2a0c5a07bf87d92d13781d753b3f7f7f68f02
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-15 08:49:46 +0800

    Delete biometric-scenario.adb

--- biometric-scenario.adb
+++
@@ -1,5 +0,0 @@
-DesiredCapabilities capabilities = new DesiredCapabilities();
-capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, “Android Emulator”);
-capabilities.setCapability(“appPackage”, “<app_package>”);
-capabilities.setCapability(“appActivity”, “<app_activity>”);
-driver = new AndroidDriver(new URL(“HTTP-PORT/wd/hub"), capabilities);
